852a9bdfc495ff87e83a5248f47dd82f4e643c98
Ben Wiedermann <benw@cs.hmc.edu>
2020-11-17 21:51:57 -0800

    initial type-checking interface

---
+++ src/typechecker.ts
@@ -0,0 +1,40 @@
+import {Position} from './position';
+import * as AST from './ast';
+
+export function typecheck(nodes: AST.Node[]): TypeError[] {
+  const errors = nodes.map(n => typecheckNode(n));
+  return ([] as TypeError[]).concat(...errors);
+}
+
+function typecheckNode(node: AST.Node): TypeError[] {
+  return checkerMap[node.type].check(node);
+}
+
+export class TypeError {
+  constructor(public message: string, public position: Position) {}
+}
+
+export interface TypeChecker {
+  check(node: AST.Node): TypeError[];
+}
+
+class CheckNumber implements TypeChecker {
+  check(node: AST.NumberNode): TypeError[] {
+    return [];
+  }
+}
+
+class CheckBinary implements TypeChecker {
+  check(node: AST.BinaryOperationNode): TypeError[] {
+    const errors: TypeError[] = typecheckNode(node.left).concat(typecheckNode(node.right));
+    if (node.left.type != node.right.type) {
+      errors.push(new TypeError("incompatible types for binary operator", node.pos));
+    }
+    return errors;
+  }
+}
+
+const checkerMap: Partial<{[K in AST.NodeType]: TypeChecker}> = {
+  'Number' : new CheckNumber(),
+  'BinaryOperation' : new CheckBinary()
+}
