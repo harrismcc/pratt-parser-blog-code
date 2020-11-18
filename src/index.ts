4347bd532039e9fd2e6958a837e7f5620f4cc43e
Ben Wiedermann <benw@cs.hmc.edu>
2020-11-17 21:52:35 -0800

    Run typechecker and display errors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import {getTokens} from './lexer';
 import {create} from './editor';
 import {parse} from './parser';
+import {typecheck} from './typechecker';
 
 const cmContainer = document.createElement('div');
 cmContainer.className = 'cm-container';
@@ -13,12 +14,20 @@ document.body.appendChild(outputContainer);
 
 function updateOutput() {
   const ast = parse(cm.getDoc().getValue());
-  cm.setOption('script-errors', ast.errors);
+  const typeErrors = typecheck(ast.nodes);
+
+  if (ast.errors.length > 0) {
+    cm.setOption('script-errors', ast.errors);
+  } else {
+    cm.setOption('script-errors', typeErrors);
+  }
 
   const tokens = getTokens(cm.getDoc().getValue());
   outputContainer.innerHTML = `\
+typeErrors: ${JSON.stringify(typeErrors, null, 2)}
 ast: ${JSON.stringify(ast, null, 2)}
-tokens: ${JSON.stringify(tokens, null, 2)}`;
+tokens: ${JSON.stringify(tokens, null, 2)}
+`;
 }
 
 cm.on('change', updateOutput);
